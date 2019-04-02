a90aa2fc866cc3331a1c9a8e8aff3e3e1976f13b
Terr1bus <qwant94@mail.ru>
2019-04-02 22:53:28 +1000

    add styles, logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Header from './components/Header';
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
-export default App;
+const App = styled.section`
+  flex-basis: 20%;
+  margin: auto;
+  padding: 20px 15px;
+  background: #fff;
+  box-shadow: 0 0 1px;
+
+`;
+
+export default function() {
+  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify([...todos]))
+  });
+  
+  const addTodo = (id, text) => {
+    setTodos([...todos, {id, text, completed: false}]);
+  };
+
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  };
+
+  const toggleTodo = id => {
+    setTodos(todos.map(todo => {
+      if (todo.id === id) {
+        return {...todo, completed: !todo.completed};
+      };
+
+      return todo;
+    }))
+  }
+
+  return (
+    <App>
+      <Header />
+      <TodoList todos={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+      <AddTodo addTodo={addTodo} />
+    </App>
+  )
+};
